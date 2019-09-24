eac1caaa31f93853684ba88c9a577047d8611ff5
stellie82 <stella@Stellas-MacBook-Pro.local>
2019-09-24 16:33:33 -0700

    add CSS background and customization, timer mechanism

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,3 @@
-
-
-
 // Set global variables for the game.
 var movieList = [{
     quote: "Wait a minute Doc.  Uh, are you telling me you built a time machine... out of a DeLorean?",
@@ -14,92 +11,103 @@ var movieList = [{
     quote: "Here's Johnny!",
     choices: ["The Shining", "Short Circuit", "The Incredibles", "Weird Science"],
     answer: "The Shining",
-},
-    // {
-    //     quote: "Hello.  My name is Inigo Montoya.  You killed my father.  Prepare to die!",
-    //     choices: ["The Princess Bride", "The Three Musketeers", "The Rocketeer", "Nacho Libre"],
-    //     answer: "The Princess Bride",
-    // }, {
-    //     quote: "Come with me if you want to live.",
-    //     choices: ["Bill & Ted's Excellent Adventure", "Stand By Me", "The Terminator", "Robocop"],
-    //     answer: "The Terminator",
-    // }, {
-    //     quote: "No such thing as bad student, only bad teacher.  Teacher say, student do.",
-    //     choices: ["Good Will Hunting", "Lean on Me", "Shawshank Redemption", "The Karate Kid"],
-    //     answer:  "The Karate Kid",
-    // }, {
-    //     quote: "Go ahead, make my millennium.",
-    //     choices: ["Sudden Impact", "Beetlejuice", "Raiders of the Lost Ark", "The Abyss"],
-    //     answer: "Beetlejuice",
-    // }, {
-    //     quote: "I just want to tell you both good luck. We're all counting on you.",
-    //     choices: ["Airplane!", "Avatar", "Titanic", "The Goonies"],
-    //     answer: "Airplane!",
-    // }, {
-    //     quote:"You wanna get nuts?  Cmon, let's get nuts!",
-    //     choices: ["Honey I Shrunk the Kids", "Roger Rabbit", "Batman", "Scarface"],
-    //     answer: "Batman",
-    // }, {
-    //     quote: "How many wishes do I get?  As many as you want. And the more wishes you make, the more magnificent Fantasia will become.",
-    //     choices: ["The Land Before Time", "The Little Mermaid", "Fantasia", "The Neverending Story"],
-    //     answer: "The Neverending Story",
-    // }, {
-    //     quote: "Ray, when someone asks you if you're a god, you say YES!",
-    //     choices: ["Ghostbusters", "Bruce Almighty", "The Fly", "Predator"],
-    //     answer: "Ghostbusters",
-    // }, {
-    //     quote: "Just fear me, love me, do as I say, and I will be your slave.",
-    //     choices: ["Poltergeist", "Labryinth", "Willow", "Gladiator"],
-    //     answer: "Labryinth",
-    // }
-];
+}, {
+    quote: "Hello.  My name is Inigo Montoya.  You killed my father.  Prepare to die!",
+    choices: ["The Princess Bride", "The Three Musketeers", "The Rocketeer", "Nacho Libre"],
+    answer: "The Princess Bride",
+}, {
+    quote: "Come with me if you want to live.",
+    choices: ["Bill & Ted's Excellent Adventure", "Stand By Me", "The Terminator", "Robocop"],
+    answer: "The Terminator",
+}, {
+    quote: "No such thing as bad student, only bad teacher.  Teacher say, student do.",
+    choices: ["Good Will Hunting", "Lean on Me", "Shawshank Redemption", "The Karate Kid"],
+    answer: "The Karate Kid",
+}, {
+    quote: "Go ahead, make my millennium.",
+    choices: ["Sudden Impact", "Beetlejuice", "Raiders of the Lost Ark", "The Abyss"],
+    answer: "Beetlejuice",
+}, {
+    quote: "I just want to tell you both good luck. We're all counting on you.",
+    choices: ["Airplane!", "Avatar", "Titanic", "The Goonies"],
+    answer: "Airplane!",
+}, {
+    quote: "You wanna get nuts?  Cmon, let's get nuts!",
+    choices: ["Honey I Shrunk the Kids", "Roger Rabbit", "Batman", "Scarface"],
+    answer: "Batman",
+}, {
+    quote: "How many wishes do I get?  As many as you want. And the more wishes you make, the more magnificent Fantasia will become.",
+    choices: ["The Land Before Time", "The Little Mermaid", "Fantasia", "The Neverending Story"],
+    answer: "The Neverending Story",
+}, {
+    quote: "Ray, when someone asks you if you're a god, you say YES!",
+    choices: ["Ghostbusters", "Bruce Almighty", "The Fly", "Predator"],
+    answer: "Ghostbusters",
+}, {
+    quote: "Just fear me, love me, do as I say, and I will be your slave.",
+    choices: ["Poltergeist", "Labryinth", "Willow", "Gladiator"],
+    answer: "Labryinth",
+}];
 
 var correctAnswers;
 var wrongAnswers;
 var unanswered;
+var seconds = 10;
+var startClock;
 
 // Create a function to start the game.
 function startGame() {
+    $("#timer").html(
+        "<p>" + "You have one minute to guess the following movies based on quotes.  To begin, press the start button.  Good luck!" + "</p>" +
+        "<button class='btn- btn-outline-pink btn-lg'>" + "START" + "</button>");
 
     correctAnswers = 0;
     wrongAnswers = 0;
     unanswered = 0;
 
-    // var countdown = setInterval(function () {
-    //     alert("Hello");
-    // }, 1000);
+    // Create a trigger to start the timer as soon as the player clicks the start button.
+    $("#timer").on("click", trigger);
+}
 
+function countdown() {
+
+    // Set the clock to count down decrementing by one second at a time.
+    startClock = setInterval(countdown, 1000);
+
+    if (seconds > 0) {
+        seconds--;
+        $("#timer").html("<h2>" + "Time Remaining: " + seconds + "</h2>");
+    }
+
+    else stop();
 }
 
 function trivia() {
     for (i = 0; i < movieList.length; i++) {
-        // console.log(i + 1);
-        // console.log(movieList[i].quote);
-        // console.log(movieList[i].choices);
+        console.log(i + 1);
+        console.log(movieList[i].quote);
+        console.log(movieList[i].choices);
 
-        $("#content").append("<div id='Question'>" + (i + 1) + "</div>");
+        // Create divs for each of the quotes and answer choices.
         $("#content").append("<div id='Quote'>" + movieList[i].quote + "</div>");
 
-        for (j = 0; j < 4; j++) {
-            // $("#content").append("<div id='Choices'>" + movieList[i].choices + "</div>");
-
-            $("#content").append("<input type='radio'>" + movieList[i].choices);
-        }
+        $.each(movieList[i].choices, function (index, key) {
+            $('#content').append($("<button class='btn- btn-outline-pink btn-sm btn-space'>" + key + "</button>"));
+        })
 
         $("#content").append("<br></br>");
     }
 }
 
-// Create a function to clear the timer.
-function stop() {
-    clearInterval(intervalId);
+// Create a function to start the countdown and display the game contents.
+function trigger() {
+    countdown();
+    trivia();
 }
 
+// Create a function to clear the timer.
+function stop() {
+    clearInterval(startClock);
+}
 
-$("document").ready(function () {
-    startGame();
-    trivia();
-});
-
-
-
+$("document").ready(startGame);
